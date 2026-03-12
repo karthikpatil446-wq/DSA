039879fc866b9f99341a4cd646385d51fcc39600
Karthik_patil_666 <karthikpatil446@gmail.com>
2026-03-12 20:10:30 +0530

    Add files via upload

---
+++ day1_reverse_list.py
@@ -0,0 +1,7 @@
+list=[4,5,8,7,4,5,7]
+list.reverse() 
+print("reverse the list:",list)  #reverse the list
+list.sort()
+print("increasing order:",list)# increasing order
+list.sort(reverse=True)
+print("decreasing order:",list)#decreasing order"""
