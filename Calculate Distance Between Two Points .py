121309b87965ebe19525a66f4370186be74b1465
Karthik_patil_666 <karthikpatil446@gmail.com>
2026-03-14 20:17:40 +0530

    Distance between two points

---
+++ Calculate Distance Between Two Points .py
@@ -0,0 +1,7 @@
+x1=float(input("enter x1 point:"))
+x2=float(input("enter x2 point:"))
+y1=float(input("enter y1 point:"))
+y2=float(input("enter y2 point:"))
+import math
+distance=math.sqrt((x2-x1)**2+(y2-y1)**2)
+print("distance 0f two points:",distance)
