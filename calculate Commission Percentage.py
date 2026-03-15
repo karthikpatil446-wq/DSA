d6202688f5da38b4a89a3b856f899bfece800901
Karthik_patil_666 <karthikpatil446@gmail.com>
2026-03-15 22:04:30 +0530

    Python program to calculate Commission ammount

---
+++ calculate Commission Percentage.py
@@ -0,0 +1,10 @@
+sales = float(input("Enter total sales amount: "))
+commission_percentage= float(input("Enter commission percentage: "))
+
+commission = (commission_percentage * sales) /100
+
+print("Commission ammount  =", commission, )
+#output
+"""Enter total sales amount: 9000
+Enter commission percentage: 10
+Commission ammount  = 900.0"""
