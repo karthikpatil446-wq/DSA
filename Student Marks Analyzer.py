6247e74cb15a1186ead4bd8f69b8a021abfe402f
Karthik_patil_666 <karthikpatil446@gmail.com>
2026-03-26 23:05:49 +0530

    Student Marks Analyzer

---
+++ Student Marks Analyzer.py
@@ -0,0 +1,11 @@
+marks=[99,78,45,140,52]
+avg=sum(marks)/len(marks)
+highest=max(marks)
+lowest=min(marks)
+print("average:",avg)
+print("highest",highest)
+print("lowest",lowest)
+#output
+#average: 82.8
+#highest 140
+#lowest 45
